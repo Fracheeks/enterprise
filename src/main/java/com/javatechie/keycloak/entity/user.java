7f69e0b0bb56fad867f4286cc1176d0102e0b772
francesca.dimella <francesca.dimella@orbyta.it>
2022-09-06 09:06:24 +0200

    add user from keycloak

--- src/main/java/com/javatechie/keycloak/entity/user.java
+++ src/main/java/com/javatechie/keycloak/entity/user.java
@@ -1,4 +1,5 @@
 package com.javatechie.keycloak.entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -20,6 +21,7 @@ public abstract class user {
     public String username;
 
     @Column(name = "userIdByToke")
+    @JsonIgnore
     public String userIdByToken; //referenced to keycloak
 
 }
